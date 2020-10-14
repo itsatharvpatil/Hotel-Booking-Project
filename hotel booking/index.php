1f273b6bc74feb49463dbb6865df402eb4ee7905
Atharv Patil <patilatharv27@hotmail.com>
2020-10-14 22:27:41 +0530

    new navbar

--- hotel booking/index.php
+++ hotel booking/index.php
@@ -52,30 +52,19 @@
         }
 		
     </style>
+
+    <link rel="stylesheet" href="styles.css">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="room.php">Room &amp; Facilities</a></li>
+      <li><a href="reservation.php">Online Reservation</a></li>
+      <li><a href="admin.php">Admin</a></li>
+  </ul>
     
     
 </head>
 
 <body>
-    <div class="container">
-      
-      
-       <img class="img-responsive" src="images/home_banner.jpg" style="width:100%; height:180px;">      
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="room.php">Room &amp; Facilities</a></li>
-                    <li><a href="reservation.php">Online Reservation</a></li>
-                    <li><a href="admin.php">Admin</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="http://www.facebook.com"><img src="images/facebook.png"></a></li>
-                    <li><a href="http://www.twitter.com"><img src="images/twitter.png"></a></li>                    
-                </ul>
-            </div>
-        </nav>
-
      
         <div class="jumbotron">
         <div class="w3-content w3-section">
@@ -108,8 +97,7 @@
             <div class="col-md-4"></div>
             <div class="col-md-4 wellfix">
                 <h4><strong>Developed By</strong></h4><hr>
-                <a href="#">@Projectworlds.in</a>
-
+                <a href="#">ATHARV, SOHAM & TANMAY</a>
             </div>
         </div>
         
